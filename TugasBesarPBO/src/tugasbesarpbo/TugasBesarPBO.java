77921860d65852becda114f1f2872e9bc093def9
dense <dense@DESKTOP-HSBUN2M>
2020-07-01 22:43:16 +0700

    Create Class named Densel

--- TugasBesarPBO/src/tugasbesarpbo/TugasBesarPBO.java
+++ TugasBesarPBO/src/tugasbesarpbo/TugasBesarPBO.java
@@ -15,7 +15,7 @@ public class TugasBesarPBO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("saya densel");
+        
     }
     
 }
