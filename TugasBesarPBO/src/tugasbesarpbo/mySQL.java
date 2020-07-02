6563c74517cb9e9dda1aff7951cd2438e594c5c1
dense <dense@DESKTOP-HSBUN2M>
2020-07-03 01:43:08 +0700

    mySQL

---
+++ TugasBesarPBO/src/tugasbesarpbo/mySQL.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasbesarpbo;
+
+/**
+ *
+ * @author dense
+ */
+public interface mySQL {
+    final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    final String DB_URL = "jdbc:mysql://localhost/data_tubes_pbo";
+    final String USER = "root";
+    final String PASS = "";
+}
