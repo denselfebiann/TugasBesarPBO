5eea58d1d8288e5a0c448664dea56ec0f0408ceb
dense <dense@DESKTOP-HSBUN2M>
2020-07-06 10:56:23 +0700

    mySQL

--- TugasBesarPBO/src/tugasbesarpbo/TugasBesarPBO.java
+++ TugasBesarPBO/src/tugasbesarpbo/TugasBesarPBO.java
@@ -15,7 +15,7 @@ public class TugasBesarPBO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new CreateDataBase();
+        new CreateTable();
     }
     
 }
