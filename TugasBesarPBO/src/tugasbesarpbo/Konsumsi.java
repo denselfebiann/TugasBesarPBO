d738dc0f2d55b6d03e28226d0755db1e65d69ba8
melvin16s <67085652+melvin16s@users.noreply.github.com>
2020-07-06 12:54:00 +0700

    Add files via upload
    Add Class

---
+++ TugasBesarPBO/src/tugasbesarpbo/Konsumsi.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasbesarpbo;
+
+/**
+ *
+ * @author MelvinS
+ */
+public class Konsumsi {
+    private int idKonsumsi;
+    private String namaProduk;
+    private int jumlah;
+    private int hargaSatuan;
+}
