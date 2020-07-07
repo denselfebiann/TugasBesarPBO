a1c111f47a682b2b493930a3083de7d0bcc9a14f
dense <dense@DESKTOP-HSBUN2M>
2020-07-07 08:54:05 +0700

    mySQL

---
+++ TugasBesarPBO/src/Model/Kereta.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author MelvinS
+ */
+public class Kereta {
+    private String idKereta;
+    private String gerbong;
+    private int jumlahKursi;
+    private boolean[] kursiKosong;
+    private ArrayList<KeretaJadwal> jadwal;
+}
