6ed7bc98389e8adf0834f498cdc50ec38b2286e7
dense <dense@DESKTOP-HSBUN2M>
2020-07-07 08:54:29 +0700

    mySQL

---
+++ TugasBesarPBO/src/Model/Pembayaran.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author MelvinS
+ */
+public class Pembayaran {
+    private String metodePembayaran;
+    private int totalHarga;
+    private int diskon;
+    private int cashback;
+}
