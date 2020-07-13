b8d1b6e208426415079b35c7f968463ee89e0767
MelvinS <>
2020-07-13 08:09:18 +0700

    commit ulang karena ga ke save

--- TugasBesarPBO/src/View/MenuMyOrders.java
+++ TugasBesarPBO/src/View/MenuMyOrders.java
@@ -5,15 +5,18 @@
  */
 package View;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 /**
  *
  * @author MelvinS
  */
-public class MenuMyOrders {
+public class MenuMyOrders implements ActionListener{
     JFrame frame = new JFrame("Menu MyOrders");
-    JLabel[] listPesanan = new JLabel[1];
+    JLabel[] listPesanan = new JLabel[3];
+    JButton back;
     
     public MenuMyOrders(){
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,19 +24,33 @@ public class MenuMyOrders {
         
         //ambil data dari db
         
-        listPesanan[0] = new JLabel("orderID | departureDipilih | tanggal | kursiDipilih | hargaTiket | langganan | konsumsi | totalHarga");
-        listPesanan[0].setBounds(20, 20, 500, 20);
+        int height = 20;
         
-//        for(int i = 0; i<listPesanan.length;i++){
-//            //print data
-//        }
         
-        frame.add(listPesanan[0]);
+        listPesanan[0] = new JLabel("orderID111 | departureDipilih | tanggal | kursiDipilih | hargaTiket | langganan | konsumsi | totalHarga");
+        listPesanan[1] = new JLabel("orderID222 | departureDipilih | tanggal | kursiDipilih | hargaTiket | langganan | konsumsi | totalHarga");
+        listPesanan[2] = new JLabel("orderID333 | departureDipilih | tanggal | kursiDipilih | hargaTiket | langganan | konsumsi | totalHarga");
+        for(int i = 0; i<listPesanan.length;i++){
+            listPesanan[i].setBounds(10, height, 600, 20);
+            frame.add(listPesanan[i]);
+            height += 20;
+        }
+        JButton back = new JButton("Back");
+        back.setBounds(20, 300, 100, 20);
+        back.addActionListener(this);
         
+        frame.add(back);
+        
+        frame.setLayout(null);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
     public static void main(String[] args) {
         new MenuMyOrders();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
